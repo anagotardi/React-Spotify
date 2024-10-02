ccd6a37a1611ccca5c7f57de2eecb92de0cb35ec
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-02 08:49:14 -0300

    02-10-24

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import conectaNaDb from './db.js';
 import cors from 'cors'; // biblioteca que permite que o react e o mongo se conectem
+import artista from './Models/Artista.js'
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,11 @@ conexao.on('error', (erro) => {
 })
 conexao.once('open', () => {
     console.log("Conectado ao MongoDB")
+})
+
+app.get('/artistas', async (req,res) =>{
+    const listaArtistas = await artista.find({});
+    res.status(200).json(listaArtistas);
 })
 
 app.listen(3000, () => {
--- server/db.js
+++ server/db.js
@@ -5,3 +5,4 @@ export default async function conectaNaDb(){
 
     return mongoose.connection;
 }
+
