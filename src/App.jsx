de7eb39f5fa0c7e6faf2a5f633b8066bcc4071c3
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 08:39:38 -0300

    16-10-24

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import CardSidebar from "./components/CardSidebar"
 import Container from "./components/Container"
 import ConteudoPrincipal from "./components/ConteudoPrincipal"
 import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
+import artista from "../server/Models/Artista"
 
 function App() {
   const[artistas, setArtistas] = useState([]);
-  const[isLoading, setIsLoading] = useState(false);
+
 
   useEffect(() => {
     fetch('http://localhost:3000/artistas')
     .then(res => res.json())
     .then(data => setArtistas(data))
     .catch(err => console.log(err))
-  })
+    .finally(() => console.log('Finalizou a requisição'))
+  },[])
  
   return (
     <>
@@ -26,15 +28,44 @@ function App() {
         <CardSidebar/>
       </Sidebar>
       <ConteudoPrincipal> 
-        {
-          artistas.map(artista => (
-            <div className="bg-rose-200 w-28 h-28 flex flex-col justify-around items-center">
-            <div className="bg-rose-300 w-3/4 h-7"></div>
-            <div className="bg-rose-300 w-3/4 h-7"></div>
+        <>
+        <h1>Pop</h1>
+       {
+        artistas
+        .filter( artista => artista.genero === "Pop")
+        .map(artista => (
+          <div className="bg-rose-200 w-28 h-28 flex flex-col justify-around items-center">
+          <h1>{artista.name}</h1>
           </div>
-          )) 
-        }
+        ))
+       }
+        </>
 
+        <>
+        <h1>Jazz</h1>
+       {
+        artistas
+        .filter( artista => artista.genero === "Jazz")
+        .map(artista => (
+          <div className="bg-rose-200 w-28 h-28 flex flex-col justify-around items-center">
+          <h1>{artista.name}</h1>
+          </div>
+        ))
+       }
+        </>
+
+        <>
+        <h1>Rap</h1>
+       {
+        artistas
+        .filter( artista => artista.genero === "Rap")
+        .map(artista => (
+          <div className="bg-rose-200 w-28 h-28 flex flex-col justify-around items-center">
+          <h1>{artista.name}</h1>
+          </div>
+        ))
+       }
+        </>
       
 
       </ConteudoPrincipal>
